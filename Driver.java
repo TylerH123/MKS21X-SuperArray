900b8ef07feba72c56bce7b069ec2df221de04c3
Tyler Huang <thuang00@stuy.edu>
2018-10-25 11:29:56 -0400

    a

--- Driver.java
+++ Driver.java
@@ -10,6 +10,7 @@ public class Driver{
     System.out.println(arr.set(0, "pen") + "\nset(0, pen) expected: null");
     System.out.println(arr + "\ntoString() expected: [pen, null, null, null, null, null, null, null, null, null, car]");
     System.out.println(arr.get(0) + "\nget(0) expected: pen");
+    System.out.println(arr.contains("pen" + "\ncontains(pen) expected: true"));
     /*
     System.out.println();
     System.out.println();*/
